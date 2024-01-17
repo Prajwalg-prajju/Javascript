9f8bc0c6279d6c1879fdc6bc742762de337286a9
Prajwal G <prajwalgprajju12@gmail.com>
2024-01-17 14:37:37 +0530

    work2

---
+++ work2.js
@@ -0,0 +1,27 @@
+// let height = '1234'
+
+// console.log(typeof(height))
+
+
+
+ // var_blank = ''
+
+ // console.log(var_blank)
+
+
+ // var_space = ''
+
+ // console.log(var_space)
+
+
+ // var_fn = 'Sachin'
+
+ // console.log(var_fn)
+
+// var_bio1 = 'Sachin Ramesh Tendulkar, is an "Indian" former'
+
+// console.log(var_bio1)
+
+var_bio2 = 'Sachin Ramesh Tendulkar, is an \'Indian\' former'
+
+console.log(var_bio2)
